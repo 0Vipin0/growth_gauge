ca4825feedac37692b57aacb30c3d81cbbad84c4
0Vipin0 <vipinmalik941@gmail.com>
2025-03-09 01:18:49 +0530

    Updated android, linux, web, windows for app data

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "growth_gauge");
+    gtk_header_bar_set_title(header_bar, "Growth Gauge");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "growth_gauge");
+    gtk_window_set_title(window, "Growth Gauge");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
